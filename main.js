0fd4d5dfe4548029e764587fb795ad166ee90788
MasterKelwin <mkelwin@outlook.com>
2022-02-03 09:06:44 -0300

    Montando listas, nada de novo até agora

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
-document.querySelector('.tecla_pom');
-document.querySelector('.tecla_clap');
-document.querySelector('tecla_tim');
-document.querySelector('tecla_puff');
-document.querySelector('tecla_splash');
-document.querySelector('tecla_toim');
-document.querySelector('tecla_psh');
-document.querySelector('tecla_tic');
-document.querySelector('tecla_tom');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 } 
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-} 
+listaDeTeclas[0].onclick = tocaSomPom;
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
