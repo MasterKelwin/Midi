1bc15853b2c9a75e689eaa0c20ea02be2f3a23c4
MasterKelwin <mkelwin@outlook.com>
2022-02-04 19:19:46 -0300

    finalizando

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 function tocaSom(idAudio){
-    document.querySelector(idAudio).play();
+    const elemento = document.querySelector(idAudio)
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+        }else{
+            alert('Elemento não encontrado ou seletor inválido');
+        }
+
 } 
 
 for (i = 0; i < listaDeTeclas.length; i++){
@@ -14,6 +21,7 @@ for (i = 0; i < listaDeTeclas.length; i++){
     }
 
     tecla.onkeydown = function(evento){
+        //console.log(evento.code)
         if(evento.code === "Enter" || evento.code === "Space"){
             tecla.classList.add('ativa');
         }
